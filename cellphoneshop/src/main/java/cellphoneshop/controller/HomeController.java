1868fcad98add5f2710e73a004d1b31cd399ba61
mkhanh <minhkhanh.khtn@gmail.com>
2013-12-07 21:52:48 +0700

    xoa hinh

---
+++ cellphoneshop/src/main/java/cellphoneshop/controller/HomeController.java
@@ -0,0 +1,13 @@
+package cellphoneshop.controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class HomeController extends ActionSupport {
+
+	@Override
+	public String execute() throws Exception {
+		
+		return SUCCESS;
+	}
+}
