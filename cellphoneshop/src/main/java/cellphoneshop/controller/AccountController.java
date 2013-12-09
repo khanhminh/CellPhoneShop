dde0b7e6ae3264fa7c08e9ccf5258a9316e1f30e
lamhaison <lamhaison@gmail.com>
2013-12-09 23:03:05 +0700

    Add thêm dependency log4j

--- cellphoneshop/src/main/java/cellphoneshop/controller/AccountController.java
+++ cellphoneshop/src/main/java/cellphoneshop/controller/AccountController.java
@@ -1,17 +1,25 @@
 package cellphoneshop.controller;
 
+//import org.slf4j.Logger;
+import org.apache.log4j.Logger;
+
+
 import com.opensymphony.xwork2.ActionSupport;
 
 @SuppressWarnings("serial")
 public class AccountController extends ActionSupport{
 	
+	
+	private Logger logger = Logger.getLogger(AccountController.class);
 	public String login(){
 		
+		logger.info("Vao login Controller AccountController");
 		return INPUT;
 	}
 	
 	public String register(){
 		
+		logger.info("Vao register Controller AccountController");
 		return INPUT;
 	}
 }
