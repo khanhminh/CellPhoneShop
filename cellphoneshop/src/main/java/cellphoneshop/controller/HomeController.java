66206d3570d51aae48fe88e0494c28d25ea6317b
lamhaison <lamhaison@gmail.com>
2013-12-09 22:48:11 +0700

    Add log4j in Project //Add vào file porm //Thêm file log4j.properties //Demo ghi log vào file trong log4j
    Signed-off-by: lamhaison <lamhaison@gmail.com>

--- cellphoneshop/src/main/java/cellphoneshop/controller/HomeController.java
+++ cellphoneshop/src/main/java/cellphoneshop/controller/HomeController.java
@@ -1,13 +1,18 @@
 package cellphoneshop.controller;
 
+
+
 import com.opensymphony.xwork2.ActionSupport;
 
 @SuppressWarnings("serial")
 public class HomeController extends ActionSupport {
+	
+	
+
 
 	@Override
 	public String execute() throws Exception {
-		
+	
 		return SUCCESS;
 	}
 }
