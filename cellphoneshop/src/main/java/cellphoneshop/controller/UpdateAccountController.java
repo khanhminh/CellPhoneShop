d5a3b62457f67b026c48777b46baadfef623798f
lamhaison <lamhaison@gmail.com>
2013-12-25 23:34:04 +0700

    Cap nhat thong tin tai khoan

--- cellphoneshop/src/main/java/cellphoneshop/controller/UpdateAccountController.java
+++ cellphoneshop/src/main/java/cellphoneshop/controller/UpdateAccountController.java
@@ -1,12 +1,21 @@
 package cellphoneshop.controller;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import cellphoneshop.model.NguoiDung;
 import cellphoneshop.security.SecurityHelper;
+import cellphoneshop.service.NguoiDungService;
+import cellphoneshop.util.Message;
 import cellphoneshop.viewmodel.UpdateUser;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -17,13 +26,63 @@ public class UpdateAccountController extends ActionSupport implements ServletReq
 	private HttpServletRequest request;
 	private UpdateUser user;
 	
-	public String updateAcccount(){
-		if (user == null){
-			return INPUT;			
+	@Autowired
+	private NguoiDungService nguoiDungService;
+	
+	@Autowired
+	private Message messages;
+	
+	private List<String> errors = new ArrayList<String>();
+	
+	
+	public String updateAcccount() {
+		errors = new ArrayList<String>();
+		if (user == null) {
+			return INPUT;
 		}
-		/// test;
-		logger.info(user.getName());
-		
+
+		if (!validateUpdate()) {
+			request.setAttribute("errors", errors);
+			return INPUT;
+		}
+
+		// / test;
+
+		NguoiDung loginUser = SecurityHelper.getUser();
+		if (loginUser == null) {
+			errors.add(messages.getMessageList().getProperty("unknowLoginUser"));
+			return INPUT;
+		}
+
+		String sex = user.getSex();
+		if (sex.equals("name")) {
+			loginUser.setGioiTinh(1);
+		} else {
+			loginUser.setGioiTinh(0);
+		}
+
+		loginUser.setDiaChi(user.getAddress());
+		Date ngaysinh = ParseDate(user.getBirthday());
+		if (ngaysinh == null) {
+			errors.add(messages.getMessageList().getProperty("errorBirthDate"));
+			request.setAttribute("errors", errors);
+			return INPUT;
+		}
+
+		loginUser.setNgaySinh(ngaysinh);
+		loginUser.setHo(user.getFirstname());
+		loginUser.setSoDienThoai(user.getPhone());
+		loginUser.setNhanTinQuaEmail(false);
+
+		if (!nguoiDungService.updateNguoidung(loginUser)) {
+			errors.add(messages.getMessageList().getProperty("updateFail"));
+			request.setAttribute("errors", errors);
+			return INPUT;
+		}
+		errors.add(messages.getMessageList().getProperty("updateSuccess"));
+		logger.info("Tai khoan " + loginUser.getEmail()
+				+ " da duoc cap nhat thong tin thanh cong!");
+		request.setAttribute("errors", errors);
 		return SUCCESS;
 	}
 	
@@ -51,5 +110,67 @@ public class UpdateAccountController extends ActionSupport implements ServletReq
 
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;		
+	}
+	
+	private boolean validateUpdate() {
+		boolean result = true;
+		errors = new ArrayList<String>();
+		try {
+
+			if (user.getFirstname().equals("")) {
+				errors.add(messages.getMessageList().getProperty(
+						"unknowFirstName"));
+				result = false;
+			}
+
+			if (user.getName().equals("")) {
+				errors.add(messages.getMessageList().getProperty("unknowName"));
+				result = false;
+			}
+
+			if (!tryParseDate(user.getBirthday())) {
+				errors.add(messages.getMessageList().getProperty(
+						"errorBirthDate"));
+				result = false;
+			}
+
+			if (!user.getPhone().matches("^\\d{6,11}$")) {
+				errors.add(messages.getMessageList().getProperty(
+						"errorPhoneNumber"));
+				result = false;
+			}
+
+			if (user.getAddress().equals("")) {
+				errors.add(messages.getMessageList().getProperty(
+						"unknowAddress"));
+				result = false;
+			}
+		} catch (Exception e) {
+			result = false;
+		}
+
+		return result;
+	}
+
+	private boolean tryParseDate(String strDate) {
+		boolean result = true;
+		DateFormat formater = new SimpleDateFormat("MM/dd/yyyy");
+		try {
+			formater.parse(strDate);
+		} catch (Exception e) {
+			result = false;
+		}
+
+		return result;
+	}
+
+	public Date ParseDate(String strDate) {
+		DateFormat formater = new SimpleDateFormat("MM/dd/yyyy");
+		try {
+			return formater.parse(strDate);
+		} catch (Exception e) {
+			logger.error("Parse date is not success");
+			return null;
+		}
 	}
 }
