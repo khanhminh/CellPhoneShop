dcfdb15e1931d1f72b4da14de68638d3f22464c2
lamhaison <lamhaison@gmail.com>
2013-12-26 00:28:31 +0700

    Xong cap nhat mat khau

--- cellphoneshop/src/main/java/cellphoneshop/controller/UpdateAccountController.java
+++ cellphoneshop/src/main/java/cellphoneshop/controller/UpdateAccountController.java
@@ -11,9 +11,12 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.dao.SaltSource;
+import org.springframework.security.authentication.encoding.PasswordEncoder;
 
 import cellphoneshop.model.NguoiDung;
 import cellphoneshop.security.SecurityHelper;
+import cellphoneshop.security.UserDetailsAdapter;
 import cellphoneshop.service.NguoiDungService;
 import cellphoneshop.util.Message;
 import cellphoneshop.viewmodel.UpdateUser;
@@ -21,20 +24,26 @@ import cellphoneshop.viewmodel.UpdateUser;
 import com.opensymphony.xwork2.ActionSupport;
 
 @SuppressWarnings("serial")
-public class UpdateAccountController extends ActionSupport implements ServletRequestAware{
+public class UpdateAccountController extends ActionSupport implements
+		ServletRequestAware {
 	Logger logger = Logger.getLogger(UpdateAccountController.class);
 	private HttpServletRequest request;
 	private UpdateUser user;
-	
+
 	@Autowired
 	private NguoiDungService nguoiDungService;
-	
+
 	@Autowired
 	private Message messages;
-	
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@Autowired
+	private SaltSource saltSource;
+
 	private List<String> errors = new ArrayList<String>();
-	
-	
+
 	public String updateAcccount() {
 		errors = new ArrayList<String>();
 		if (user == null) {
@@ -51,6 +60,7 @@ public class UpdateAccountController extends ActionSupport implements ServletReq
 		NguoiDung loginUser = SecurityHelper.getUser();
 		if (loginUser == null) {
 			errors.add(messages.getMessageList().getProperty("unknowLoginUser"));
+			request.setAttribute("errors", errors);
 			return INPUT;
 		}
 
@@ -79,39 +89,75 @@ public class UpdateAccountController extends ActionSupport implements ServletReq
 			request.setAttribute("errors", errors);
 			return INPUT;
 		}
-		errors.add(messages.getMessageList().getProperty("updateSuccess"));
+		// errors.add(messages.getMessageList().getProperty("updateSuccess"));
 		logger.info("Tai khoan " + loginUser.getEmail()
 				+ " da duoc cap nhat thong tin thanh cong!");
-		request.setAttribute("errors", errors);
+
+		// Set bien isSuccess = true - thanh cong
+		request.setAttribute("isSuccess", true);
 		return SUCCESS;
 	}
-	
-	public String changePassword(){
+
+	public String changePassword() {
 		String currentPassword = request.getParameter("currentPassword");
 		String newPassword = request.getParameter("newPassword");
 		String confirmPassword = request.getParameter("confirmPassword");
-		
+		errors = new ArrayList<String>();
+
+		NguoiDung loginUser = SecurityHelper.getUser();
+		if (loginUser == null) {
+			errors.add(messages.getMessageList().getProperty("unknowLoginUser"));
+			request.setAttribute("errors", errors);
+			return INPUT;
+		}
+
 		// test
-		if (currentPassword != null){
-			logger.info(currentPassword);
+		if (currentPassword == null) {
+			return SUCCESS;
+		}
+
+		if (!newPassword.equals(confirmPassword)) {
+			errors.add(messages.getMessageList().getProperty("mismatch"));
+			request.setAttribute("errors", errors);
+			return SUCCESS;
 		}
 		
-		return SUCCESS;
-	}	
+		Object salt = saltSource.getSalt(new UserDetailsAdapter(loginUser));
+		String password = passwordEncoder.encodePassword(currentPassword, salt);
+		if(loginUser.getMatKhau().equals(password)){
+			password = passwordEncoder.encodePassword(newPassword, salt);
+			loginUser.setMatKhau(password);
+			if(nguoiDungService.updateNguoidung(loginUser)){
+				request.setAttribute("isSuccess", true);
+				logger.info("Update thanh cong mat khau cho email: " + loginUser.getEmail());
+				return SUCCESS;
+				
+			}else{
+				 errors.add(messages.getMessageList().getProperty("updatelFailPassword"));
+				 request.setAttribute("errors", errors);
+				 return SUCCESS;
+			}
+			
+		}else{
+			 errors.add(messages.getMessageList().getProperty("errorCurrentPassword"));
+			 request.setAttribute("errors", errors);
+			 return SUCCESS;
+		}
 	
-	
+	}
+
 	public void setUser(UpdateUser user) {
 		this.user = user;
 	}
-	
+
 	public UpdateUser getUser() {
 		return user;
 	}
 
 	public void setServletRequest(HttpServletRequest request) {
-		this.request = request;		
+		this.request = request;
 	}
-	
+
 	private boolean validateUpdate() {
 		boolean result = true;
 		errors = new ArrayList<String>();
