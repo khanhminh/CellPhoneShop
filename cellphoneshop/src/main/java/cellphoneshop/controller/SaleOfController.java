0e9752516cc28fcd7b185ca73de6cb5a284abf89
lamhaison <lamhaison@gmail.com>
2013-12-30 13:33:58 +0700

    Dao Quan ly khuyen mai, hien thi khuyen mai

--- cellphoneshop/src/main/java/cellphoneshop/controller/SaleOfController.java
+++ cellphoneshop/src/main/java/cellphoneshop/controller/SaleOfController.java
@@ -5,44 +5,68 @@ import java.io.File;
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cellphoneshop.model.KhuyenMai;
+import cellphoneshop.service.KhuyenMaiService;
 
 import java.io.IOException;
-
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
 import com.opensymphony.xwork2.ActionSupport;
 
 @SuppressWarnings("serial")
-public class SaleOfController extends ActionSupport implements ServletRequestAware{
+public class SaleOfController extends ActionSupport implements
+		ServletRequestAware {
 	private File myFile;
 	private String myFileContentType;
 	private String saveDirectory;
+	private Integer saleOfPerPage;
 	private String myFileFileName;
 	private String destPath;
 	private HttpServletRequest request;
-	
-	
 
 	private Logger log = Logger.getLogger(SaleOfController.class);
+	@Autowired
+	private KhuyenMaiService khuyenMaiService;
 
 	public String execute() {
-//		HttpServletRequest request = ServletActionContext.getRequest();
-		this.saveImage();
+		log.info("Vao ham execute controller");
+		Integer curentPage = getPageNumber(request.getParameter("page"));
+		List<KhuyenMai> khuyenmailList = this.getListKhuyenMail(curentPage);
+		for(KhuyenMai km : khuyenmailList){
+			log.info("Mo ta: " + km.getMoTa());
+		}
+		request.setAttribute("kmList", khuyenmailList);
+		
 		return SUCCESS;
 	}
-	
-	public boolean saveImage(){
-		destPath = request.getSession().getServletContext().getRealPath(saveDirectory);
 
-		try {			
+	public String insertKhuyenMai() {
+		// HttpServletRequest request = ServletActionContext.getRequest();
+		// this.saveImage();
+		log.info("Vao ham insert khuyen mai controller");
+
+		return SUCCESS;
+	}
+
+	public boolean saveImage() {
+		destPath = request.getSession().getServletContext()
+				.getRealPath(saveDirectory);
+
+		try {
 			File destFile = new File(destPath, myFileFileName);
 			FileUtils.copyFile(myFile, destFile);
-			log.info("Upload comlete, link image save on database is " + this.getLinkImage());
+			log.info("Upload comlete, link image save on database is "
+					+ this.getLinkImage());
 			return true;
 
 		} catch (IOException ex) {
-			log.error("Can't upload file " + this.myFileFileName + ex.getMessage());
+			log.error("Can't upload file " + this.myFileFileName
+					+ ex.getMessage());
 			return false;
 		}
 	}
@@ -73,7 +97,7 @@ public class SaleOfController extends ActionSupport implements ServletRequestAwa
 
 	public void setServletRequest(HttpServletRequest request) {
 		this.request = request;
-		
+
 	}
 
 	public String getSaveDirectory() {
@@ -83,8 +107,36 @@ public class SaleOfController extends ActionSupport implements ServletRequestAwa
 	public void setSaveDirectory(String saveDirectory) {
 		this.saveDirectory = saveDirectory;
 	}
-	
-	public String getLinkImage(){
+
+	public String getLinkImage() {
 		return this.saveDirectory + "/" + this.myFileFileName;
+	}
+
+	public Integer getSaleOfPerPage() {
+		return saleOfPerPage;
+	}
+
+	public void setSaleOfPerPage(Integer saleOfPerPage) {
+		this.saleOfPerPage = saleOfPerPage;
+	}
+
+	public Integer getPageNumber(String page) {
+		if (page == null || page.equals("")) {
+			return 1;
+		}
+
+		try {
+			return Integer.parseInt(page);
+
+		} catch (Exception e) {
+			log.error("Parse page error" + page);
+			return 1;
+
+		}
+	}
+	
+	public List<KhuyenMai> getListKhuyenMail(Integer page){
+		Integer vitriBD = (page - 1) * this.saleOfPerPage;
+		return khuyenMaiService.getListKhuyenMai(vitriBD, this.saleOfPerPage);
 	}
 }
